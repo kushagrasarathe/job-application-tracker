2222082fbffb2055093553144bd123ba324934d7
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-08-26 21:55:55 +0530

    added layout

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <div>
+          <h2>Job Application Tracker</h2>
+        </div>
+        <div>
+            <ul className={styles.navlink}>
+                <li>Home</li>
+                <li>Account</li>
+            </ul>
+        </div>
+      </nav>
+      {children}
+      <footer className={styles.footer}>
+        <span>Built by </span>
+        <a
+          href="https://twitter.com/kushagrasarathe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <b>
+            <u>@kushagrasarathe</u>
+          </b>
+        </a>
+      </footer>
+    </>
+  );
+}
